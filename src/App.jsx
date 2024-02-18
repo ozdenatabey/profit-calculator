5d7f9d58eb9e624e4fe7a50a512d237a07f032e9
Özden Atabey <ozdenatabey44@gmail.com>
2024-02-18 13:15:35 +0300

    updated background, font and added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  <body>
-    <h1>Hello</h1>
-  </body>
+  return (
+    <body>
+      <header id="header">
+        <img src="src/assets/invenstment.png" alt="investing" />
+        <h1>Profit Calculator App</h1>
+      </header>
+      <h2>asdasdad</h2>
+      <h3>Third</h3>
+      <p>Phragraph</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Eum voluptatibus numquam totam molestias?</p>
+      <p>Sint quis modi ratione provident.</p>
+      <p>Voluptate numquam enim exercitationem voluptates?</p>
+      <p>Dignissimos sunt natus nemo recusandae.</p>
+      <p>Doloremque voluptas tempora obcaecati deleniti?</p>
+      <p>Velit delectus quisquam magnam architecto.</p>
+      <p>Ex, adipisci aut. Culpa, pariatur.</p>
+      <p>Odio voluptates eos aliquam quibusdam.</p>
+      <p>Hic vitae repudiandae optio beatae!</p>
+      <p>Pariatur assumenda natus corrupti possimus!</p>
+      <p>Accusantium temporibus expedita autem eius.</p>
+      <p>Ut tenetur dignissimos optio sint.</p>
+      <p>Quam est ad distinctio harum!</p>
+      <p>Eaque a voluptatem id consequatur?</p>
+      <p>Velit possimus illo porro voluptatum.</p>
+      <p>Iste dolores corporis facere eveniet?</p>
+      <p>Quisquam, itaque. Voluptas, numquam nobis.</p>
+      <p>Quo maxime illum sequi voluptate.</p>
+      <p>Aperiam laudantium magni aut dolorum.</p>
+    </body>
+  );
 }
-export default App
+export default App;
