66c284668837398d95b0a6b03a3e2578caeb5f08
Özden Atabey <ozdenatabey44@gmail.com>
2024-02-20 23:55:21 +0300

    Added form block of input values

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,23 @@ function App() {
         <img src="src/assets/invenstment.png" alt="investing" />
         <h1>Profit Calculator App</h1>
       </header>
-      <h2>asdasdad</h2>
-      <h3>Third</h3>
-      <p>Phragraph</p>
-      <p>Lorem ipsum dolor sit amet.</p>
-      <p>Eum voluptatibus numquam totam molestias?</p>
-      <p>Sint quis modi ratione provident.</p>
-      <p>Voluptate numquam enim exercitationem voluptates?</p>
-      <p>Dignissimos sunt natus nemo recusandae.</p>
-      <p>Doloremque voluptas tempora obcaecati deleniti?</p>
-      <p>Velit delectus quisquam magnam architecto.</p>
-      <p>Ex, adipisci aut. Culpa, pariatur.</p>
-      <p>Odio voluptates eos aliquam quibusdam.</p>
-      <p>Hic vitae repudiandae optio beatae!</p>
-      <p>Pariatur assumenda natus corrupti possimus!</p>
-      <p>Accusantium temporibus expedita autem eius.</p>
-      <p>Ut tenetur dignissimos optio sint.</p>
-      <p>Quam est ad distinctio harum!</p>
-      <p>Eaque a voluptatem id consequatur?</p>
-      <p>Velit possimus illo porro voluptatum.</p>
-      <p>Iste dolores corporis facere eveniet?</p>
-      <p>Quisquam, itaque. Voluptas, numquam nobis.</p>
-      <p>Quo maxime illum sequi voluptate.</p>
-      <p>Aperiam laudantium magni aut dolorum.</p>
+      <div className="input-group" id="input-container">
+        <form>
+          <label>Entry Price</label>
+          <br />
+          <input placeholder="Enter price" type="text" />
+        </form>
+        <form>
+          <label>Goal Profit</label>
+          <br />
+          <input placeholder="Enter daily profit %" type="text" />
+        </form>
+        <form>
+          <label>Date Range</label>
+          <br />
+          <input placeholder="Enter number of days" type="text" />
+        </form>
+      </div>
     </body>
   );
 }
